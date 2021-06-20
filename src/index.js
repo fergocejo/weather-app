3c6f6532f3d85eac7d7b1c16a68238c19ea20a41
fergocejo <44611972+fergocejo@users.noreply.github.com>
2021-06-20 11:35:25 -0500

    added forecast JS

--- src/index.js
+++ src/index.js
@@ -115,6 +115,34 @@ function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sunday", "Monday", "Tuesday"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `    
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+          `;
+  });
+
+  forecastHTML = forecastHTML + `<div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let newDay = document.querySelector("#date");
 let currentTime = new Date();
 newDay.innerHTML = changeDay(currentTime);
@@ -132,3 +160,5 @@ farenheit.addEventListener("click", convertF);
 
 let button = document.querySelector("#currentTemp");
 button.addEventListener("click", getCurrentPosition);
+
+displayForecast();
